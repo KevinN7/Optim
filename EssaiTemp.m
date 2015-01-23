b8a875fbab3634c00750f852fe9cf8a585fb4026
KevinN7 <eccli.kevin@gmail.com>
2015-01-23 23:56:25 +0100

    b

--- EssaiTemp.m
+++ EssaiTemp.m
@@ -1,21 +1,36 @@
-[x,fval,exitflag,output] = fminunc('f1',[3;5])
-%[x,fval,exitflag,output] = fminunc(f1,[3;5])
-[x,fval,exitflag,output] = fminunc(@f1,[3;5])
+global nfev;
+global ngev;
+global nhev;
+global nin;
+global nout;
 
-% 
-% output
-% Structure containing information about the optimization. The fields of the structure are
-% iterations	
-% Number of iterations taken
-% funcCount	
-% Number of function evaluations
-% firstorderopt	
-% Measure of first-order optimality
-% algorithm	
-% Optimization algorithm used
-% cgiterations	
-% Total number of PCG iterations ('trust-region' algorithm only)
-% stepsize	
-% Final displacement in x ('quasi-newton' algorithm only)
-% message	
-% Exit message
+%INIT
+
+%Paramere probleme
+f = 'f1';g = 'g1';h = 'h1';
+x0 = [10;10];
+H0 = eye(2);
+
+%Parametre recherche lineaire
+rho = 0.5;  %contraction backtracking
+c1 =0.5;  %wolf1
+c2 =0.7;  %wolf2
+s0 = 0.5; %interpolation
+
+%Critere Arret
+itmax = 10^6;
+epsi = 10^-6;
+
+nfev = 0;
+ngev = 0;
+nhev = 0;
+nin = 0;
+nout = 0;
+tic;
+[xmin,fx,flag] = newtonLocale(f,g,h,x0,epsi,itmax);
+time = toc;
+fprintf('Temps:%f\n',time);
+fprintf('Iteration externe:%d  Iteration interne:%d  \n',nout,nin);
+fprintf('Appel fct:%d  Appel grad:%d  Appel Hessien:%d  \n',nfev,ngev,nhev);
+traiterFlag(flag)
+xmin
